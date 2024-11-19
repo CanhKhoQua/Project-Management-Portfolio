6123786b8b90c149d06417e535d47a4788fce608
Loc Nguyen <loc.nguyen.leo@gmail.com>
2024-11-19 16:15:05 -0500

    refine

---
+++ templates/wbs.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def display_wbs():
+    # Replace with the path to your WBS image
+    st.image("static\images\Project WBS.png", caption="Work Breakdown Structure", use_column_width=True)
+
+if __name__ == "__main__":
+    display_wbs()
